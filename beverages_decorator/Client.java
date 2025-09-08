f496f49da8fd73273fa6f9eb3ba8eeb3e6afc9d8
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-09-08 15:29:10 +0530

    feat: solve for bevarages_decorator

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -3,15 +3,12 @@ package beverages_decorator;
 public class Client {
 
 	public static void main(String[] args) {
-		
-
-		
 		Beverage coffee = new Cappuccino();
-        System.out.println("Coffee");
+		System.out.println("Coffee");
 		System.out.println(coffee.cost());
-		
-		
-
+		coffee = new Latte(coffee);
+		System.out.println("Coffee + Latte");
+		System.out.println(coffee.cost());
 	}
 
 }
